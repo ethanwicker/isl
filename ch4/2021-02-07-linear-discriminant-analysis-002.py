b27242753a9c8320aff4c66cf5b999dc39592ad2
Ethan Wicker <ethanwicker@gmail.com>
2021-02-08 09:11:11 -0700

    Backing up post

--- ch4/2021-02-07-linear-discriminant-analysis-002.py
+++ ch4/2021-02-07-linear-discriminant-analysis-002.py
@@ -15,10 +15,13 @@
 # Consider just using the titanic dataset again, would be a simple comparison again logistic regression
 
 import janitor
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
 
 titanic = pd.read_csv("~/python/isl/data/titanic-train-kaggle.csv")
 
@@ -47,9 +50,50 @@ lda.fit(X, y)
 lda.predict(X)
 lda.score(X, y)
 
-# X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-# y = np.array([1, 1, 1, 2, 2, 2])
-# clf = LinearDiscriminantAnalysis()
-# clf.fit(X, y)
-# print(clf.predict([[-0.8, -1]]))
-# clf.score(X, y)
+##########
+
+# LDA with p > 1
+
+# Because of the assumption that X = (X_1, X_2, .., X_p) is drawn from a
+# multivariate Gaussian distribution, categorical predictors don't play well with
+# LDA.  So will use only quantitative predictors.
+
+# Also LDA is somewhat robust to non-Gaussian distributions, but is not guarenteed
+# to find an optimal solution.  I'll show the distribution of age and far, and comment on this,
+# but won't do anything to correct any non-gaussian distributions.
+
+# Showing distribution of age and fare
+# kde = Kernel density estimation
+# Both are skewed right, but will ignore this for now
+fig, axes = plt.subplots(1, 2)
+sns.histplot(titanic["age"], kde=True, color="skyblue", ax=axes[0])
+sns.histplot(titanic["fare"], kde=True, color="olive", ax=axes[1])
+
+X = titanic[["age", "fare"]]
+
+lda = LinearDiscriminantAnalysis()
+lda.fit(X, y)
+lda.predict(X)
+lda.score(X, y)   # actually worse than the one above
+
+##########
+
+# Comparing the same predictors using logistic regression
+
+log_reg = LogisticRegression(penalty="none")
+log_reg.fit(X, y)
+log_reg.predict(X)
+log_reg.score(X, y)  # slightly better than LDA
+
+##########
+
+# Comment that both the scores above are on training data, and thus inflated
+
+# the null classifier that predicts every dies always is right 0.594 of the time
+sum(titanic["survived"] == 0) / len(titanic)   # 0.594
+
+
+# Discuss false positive rates (type I error, 1 - specificity), true positive rate (sensitivity, recall), precision
+
+# Show a ROC/AUC comparison to determine which is the better classifer (higher AUC)
+
