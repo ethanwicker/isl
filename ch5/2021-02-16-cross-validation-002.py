720786ef501c48bfe1d92f53f068656f5e789d1c
Ethan Wicker <ethanwicker@gmail.com>
2021-02-17 09:11:58 -0700

    Backing up notes

--- ch5/2021-02-16-cross-validation-002.py
+++ ch5/2021-02-16-cross-validation-002.py
@@ -30,7 +30,7 @@
 # 0.96...
 
 # Discuss cross_val_score
-# When the cv argument is an integer, cross_val_score uses the KFold or StratifiedKFold strategies by default,
+# When the cv argument is an integer, cross_val_score uses the KFold or StratifiedKFold strategies by default, (think StratifiedKFold here if doing classifier)
 # talk about pipelines and using to prevent data leak
 # the cross_validate function
 # KFold
@@ -39,3 +39,91 @@
 # GroupKFold
 # TimeSeriesSplit
 # mention many more can be found here: https://scikit-learn.org/stable/modules/cross_validation.html#group-cv
+
+#####
+# Start below
+#####
+
+# • Explain what cross validation is
+# • In a previous post, I introduced much of the theory behind CV
+# • In this post, I'll explore some working example using CV from scikit-learn
+# • In particular, I'll explore simple validation sets using train_test_split, `KFold` CV, `LeaveOneOut`, `StratifiedKFold` CV,
+# • GroupKFold   # will just discuss this, won't actually do it
+# • TimeSeriesSplit    # will just discuss this, won't actually do it
+# • mention many more can be found here: https://scikit-learn.org/stable/modules/cross_validation.html#group-cv
+
+## Note on scaling:
+# For logistic regression (non-regularized), LDA, and QDA, I shouldn't need to scale my observations
+# But going to anyway just because it typically doesn't hurt
+# And compare results afterwards to see if equal or not
+# Also all of Iris in the same units (cm)
+##
+
+####
+# Getting Iris Dataset (classification task)
+####
+
+import pandas as pd
+
+from sklearn.datasets import load_iris
+
+# X, y = load_iris(return_X_y=True)
+
+# Loading Iris as pandas DataFrame
+iris = load_iris(as_frame=True)
+
+# Creating target_names DataFrame to match up target with target_name
+iris_target_names = pd.DataFrame(data=dict(target=[0, 1, 2], target_name=iris.target_names))
+
+# Merging
+data = (iris["frame"]
+        .merge(right=iris_target_names,
+               on="target",
+               how="left"))
+
+####
+# Plot Iris as a scatterplot and show all the classes with different colors
+####
+
+import seaborn as sns
+
+sns.scatterplot(x="sepal length (cm)",
+                y="sepal width (cm)",
+                hue="target_name",
+                data=data)
+
+####
+# Try LDA by itself, don't scale, and use train_test_split
+####
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+
+# Loading Iris again, returning X and y as NumPy arrays
+X, y = load_iris(return_X_y=True)
+
+# Simple validation set split
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.25,
+                                                    random_state=1234,
+                                                    shuffle=True)
+
+lda = LinearDiscriminantAnalysis()
+lda.fit_transform(X=X, y=y)  # I don't think y here is being considered a categorical variable
+
+####
+# Creating a pipeline to scale data and then train log reg, quadratic log reg, LDA, QDA with KFold/really Stratified KFold
+# Logistic regression here is going to be a one-vs-rest multi-class logistic regression classifier
+####
+
+####
+# Same as above with LeaveOneOut
+####
+
+####
+# Discuss GroupKFold, TimeSeriesSplit, the above link
+####
+
+
+
+
