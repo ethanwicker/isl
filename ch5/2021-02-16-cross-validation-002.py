ddf4b7764afdc20be5b4903f0fad56f07cbfb271
Ethan Wicker <ethanwicker@gmail.com>
2021-02-18 09:12:59 -0700

    Backing up notes

--- ch5/2021-02-16-cross-validation-002.py
+++ ch5/2021-02-16-cross-validation-002.py
@@ -63,6 +63,7 @@
 # Getting Iris Dataset (classification task)
 ####
 
+import numpy as np
 import pandas as pd
 
 from sklearn.datasets import load_iris
@@ -112,19 +113,109 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 lda = LinearDiscriminantAnalysis()
 lda.fit(X=X_train, y=y_train)
 
-lda.predict(X_train) # training predictions
-lda.predict(X_test)  # training predictions
+lda.predict(X_train)  # training predictions
+lda.predict(X_test)   # training predictions
 lda.score(X=X_train, y=y_train)  # training error rate
 lda.score(X=X_test, y=y_test)    # test error rate
 
-
-
-
 ####
 # KFold/really Stratified KFold
-# Creating a pipeline to scale data and then train log reg, quadratic log reg, LDA, QDA with KFold/really Stratified KFold
+# But also do some by hand for practice and such, and future notes to myself
+# Create a pipeline to scale data and then train log reg, quadratic log reg, LDA, QDA with KFold/really Stratified KFold
 # Logistic regression here is going to be a one-vs-rest multi-class logistic regression classifier
 ####
+
+from sklearn.model_selection import cross_val_score
+lda = LinearDiscriminantAnalysis()
+# This time can use the entirety of X and y
+# 5-fold k-fold cross-validation by default, doing 10-fold below
+scores = cross_val_score(lda, X, y, cv=10)   # Performing stratified k-Fold CV here --> keeping class labels roughly same in each fold
+scores # view the scores
+scores.mean()
+scores.std()
+
+# We can also repeat the above stratified k-fold procedure multiple times
+# Since folds are random,
+# this gives us a better idea of the underlying structure true test error we are attempting to estimate
+# Also more stable results when comparing models
+from sklearn.model_selection import RepeatedStratifiedKFold
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1234)
+scores = cross_val_score(lda, X, y, cv=cv)
+scores
+scores.mean()
+scores.std()
+
+mean_cv_scores = pd.DataFrame(data=scores.reshape(10, 5)).mean()
+pd.DataFrame(data=dict(mean_cv_score=mean_cv_scores,
+                       model="Linear Discriminant Analysis",
+                       repeat=["Repeat " + str(i) for i in range(1, 6)]))
+
+
+# I can use this to perform the quadratic logistic regression in a pipeline just for quadratic logistic regression
+poly = PolynomialFeatures(degree=2, interaction_only=False, include_bias=False)
+X_poly = poly.fit_transform(X_train)
+X_poly.shape
+
+
+# I want pipeline steps for
+# LDA
+# QDA
+# Logistic Regression            --> set the necessary argument to "none"
+# Quadratic Logistic Regression  --> in this one use Polynomial Features step & set the necessary argument to "none"
+
+# step_simple_imputer = ("simple_imputer", SimpleImputer(strategy="constant"))
+# step_encoder = ("encoder", OneHotEncoder(sparse=False, handle_unknown="ignore"))
+#
+# # Creating pipeline
+# pipeline = Pipeline([step_simple_imputer, step_encoder])
+
+from sklearn.pipeline import Pipeline
+pipelines = list()
+pipeline_lda = ("lda", (Pipeline([("lda", LinearDiscriminantAnalysis())])))
+pipeline_qda = ("qda", Pipeline([("qda", QuadraticDiscriminantAnalysis())]))
+pipeline_log_reg = ("log_reg", Pipeline([("log_reg", LogisticRegression())]))
+pipeline_quadratic_log_reg = ("quadratic_log_reg",
+                              Pipeline([
+                                  # insert polynomial features stuff here
+                                  # then fit
+                                  # do this outside the pipeline first, then put it in here
+                                  ("log_reg", LogisticRegression()),
+
+                              ]))
+
+# this is what i want
+# It's a list of pipelines that is then iterated through
+# From here: https://www.kaggle.com/rakesh2711/multiple-models-using-pipeline
+pipelines = []
+pipelines.append(('scaledLR', (Pipeline([('scaled', StandardScaler()), ('LR', LogisticRegression())]))))
+pipelines.append(('scaledKNN', (Pipeline([('scaled', StandardScaler()), ('KNN', KNeighborsClassifier())]))))
+pipelines.append(('scaledDT', (Pipeline([('scaled', StandardScaler()), ('DT', DecisionTreeClassifier())]))))
+pipelines.append(('scaledSVC', (Pipeline([('scaled', StandardScaler()), ('SVC', SVC())]))))
+pipelines.append(('scaledMNB', (Pipeline([('scaled', StandardScaler()), ('MNB', GaussianNB())]))))
+
+model_name = []
+results = []
+for pipe, model in pipelines:
+    kfold = KFold(n_splits=10, random_state=42)
+    crossv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(crossv_results)
+    model_name.append(pipe)
+    msg = "%s: %f (%f)" % (model_name, crossv_results.mean(), crossv_results.std())
+    print(msg)
+
+# Compare different Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(model_name)
+plt.show()
+
+
+# Comparing with
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+
 
 # Something like this what I want
 # From: https://stackoverflow.com/questions/51629153/more-than-one-estimator-in-gridsearchcvsklearn
