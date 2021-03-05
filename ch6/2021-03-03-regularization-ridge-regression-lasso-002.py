485849c1442d23fe2ab70fc7fa1d93b7cb52ca66
Ethan Wicker <ethanwicker@gmail.com>
2021-03-04 21:04:44 -0700

    Backing up notes

--- ch6/2021-03-03-regularization-ridge-regression-lasso-002.py
+++
@@ -1,4 +0,0 @@
-# This blog has a great example of making the plot:
-# https://towardsdatascience.com/ridge-regression-for-better-usage-2f19b3a202db
-# show the comparison to statsmodels https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.OLS.fit_regularized.html
-
---
+++ ch6/2021-03-04-regularization-ridge-regression-lasso-002.py
@@ -0,0 +1,13 @@
+# This blog has a great example of making the plot:
+# https://towardsdatascience.com/ridge-regression-for-better-usage-2f19b3a202db
+# show the comparison to statsmodels https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.OLS.fit_regularized.html
+
+# Boston housing data
+# Grid search example of best C value for ridge
+# then evaludate model via cross-validation with that C on all data
+# Show the plot
+
+# Repeat for lasso
+# Maybe show the plot
+
+# Show either or in statsmodels, and comment that the other is just changing one value
